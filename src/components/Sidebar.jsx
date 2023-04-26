eb6b13bc4d8da1945a29d10264c38ed0053d4f00
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2023-04-26 23:09:19 +0100

    fixed scrollbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,32 +9,15 @@ import { BsBoxArrowLeft, BsBoxArrowRight } from "react-icons/bs";
 export default function Sidebar({ isCollapisible, setCollapisible }) {
   const router = useRouter();
   const currentRoute = router.pathname;
-
-  return (
-    <div
-      className={`${
-        isCollapisible ? "-translate-x-6" : "translate-x-50"
-      } transition ease-in duration-250 fixed left-0 
-    z-30 pt-4 pb-8 flex flex-col justify-between h-screen pl-2 pr-4 border-gray border-r-[1px]`}
-    >
-      <div>
-        {!isCollapisible ? (
-          <div className=" flex items-center justify-between">
-            {" "}
-            <Link href={"/"} className="py-2 pl-10 block">
-              <Image
-                src="/images/mainstack-logo.svg"
-                width={30}
-                height={30}
-                alt="logo"
-              />{" "}
-            </Link>{" "}
-            <BsBoxArrowLeft
-              className=" hover:text-primary cursor-pointer"
-              onClick={() => setCollapisible(true)}
-            />
-          </div>
-        ) : (
+  const SmallSizebar = () => {
+    return (
+      <div
+        className={`${
+          isCollapisible ? "w-20 block" : "hidden"
+        } transition ease-in duration-250 fixed left-0 
+      z-30 pt-4 pb-8 flex flex-col justify-between h-screen pl-2 pr-4 border-gray border-r-[1px]`}
+      >
+        <div>
           <div className="py-2 pl-10 ">
             {" "}
             <BsBoxArrowRight
@@ -42,66 +25,153 @@ export default function Sidebar({ isCollapisible, setCollapisible }) {
               onClick={() => setCollapisible(false)}
             />
           </div>
-        )}
-
-        <div className="">
-          {navigationItem.map(({ id, subitem, description }) => {
-            return (
-              <div className="flex text-gray-400 flex-col gap-2 my-4" key={id}>
-                <p
-                  className={`${
-                    isCollapisible ? "hidden" : "block"
-                  } pl-10 text-[11px]`}
-                >
-                  {description}
-                </p>
-                <div className="flex flex-col gap-2">
-                  {subitem.map(({ id, title, Icon, link }) => {
-                    return (
-                      <Link
-                        href={link}
-                        key={id}
-                        className={`my-1.5 border-2 font-medium text-[14px]   px-8 border-white ${
-                          currentRoute === link &&
-                          "text-primary border-l-primary border-l-2"
-                        } hover:text-primary hover:border-l-primary hover:border-l-2`}
-                      >
-                        <Icon className="inline mr-2" />
-                        <p
-                          className={`${
-                            isCollapisible ? "hidden" : "block"
-                          } inline`}
+  
+          <div className="">
+            {navigationItem.map(({ id, subitem, description }) => {
+              return (
+                <div className="flex text-gray-400 flex-col gap-2 my-4" key={id}>
+              
+                  <div className="flex flex-col gap-2">
+                    {subitem.map(({ id, title, Icon, link }) => {
+                      return (
+                        <Link
+                          href={link}
+                          key={id}
+                          className={`my-1.5 border-2 font-medium text-[14px]   px-8 border-white ${
+                            currentRoute === link &&
+                            "text-primary border-l-primary border-l-2"
+                          } hover:text-primary hover:border-l-primary hover:border-l-2`}
                         >
-                          {title}
-                        </p>
-                      </Link>
-                    );
-                  })}
+                          <Icon className="inline mr-2" />
+                        </Link>
+                      );
+                    })}
+                  </div>
                 </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="pl-10 flex items-center justify-between">
+          <div className="">
+            <Image
+              width={30}
+              height={30}
+              src="/images/userimage.png"
+              alt="user"
+              className="inline mr-3"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  return (
+    <>
+      {isCollapisible ? (
+        <SmallSizebar
+          isCollapisible={isCollapisible}
+          setCollapisible={setCollapisible}
+        />
+      ) : (
+        <div
+          className={`${
+            isCollapisible ? "hidden" : ""
+          } w-60 transition ease-in duration-250 fixed left-0 
+    z-30 pt-4 pb-8 flex flex-col justify-between h-screen pl-2 pr-4 border-gray border-r-[1px]`}
+        >
+          <div>
+            {!isCollapisible ? (
+              <div className=" flex items-center justify-between">
+                {" "}
+                <Link href={"/"} className="py-2 pl-10 block">
+                  <Image
+                    src="/images/mainstack-logo.svg"
+                    width={30}
+                    height={30}
+                    alt="logo"
+                  />{" "}
+                </Link>{" "}
+                <BsBoxArrowLeft
+                  className=" hover:text-primary cursor-pointer"
+                  onClick={() => setCollapisible(true)}
+                />
               </div>
-            );
-          })}
+            ) : (
+              <div className="py-2 pl-10 ">
+                {" "}
+                <BsBoxArrowRight
+                  className=" hover:text-primary cursor-pointer"
+                  onClick={() => setCollapisible(false)}
+                />
+              </div>
+            )}
+
+            <div className="">
+              {navigationItem.map(({ id, subitem, description }) => {
+                return (
+                  <div
+                    className="flex text-gray-400 flex-col gap-2 my-4"
+                    key={id}
+                  >
+                    <p
+                      className={`${
+                        isCollapisible ? "hidden" : "block"
+                      } pl-10 text-[11px]`}
+                    >
+                      {description}
+                    </p>
+                    <div className="flex flex-col gap-2">
+                      {subitem.map(({ id, title, Icon, link }) => {
+                        return (
+                          <Link
+                            href={link}
+                            key={id}
+                            className={`my-1.5 border-2 font-medium text-[14px]   px-8 border-white ${
+                              currentRoute === link &&
+                              "text-primary border-l-primary border-l-2"
+                            } hover:text-primary hover:border-l-primary hover:border-l-2`}
+                          >
+                            <Icon className="inline mr-2" />
+                            <p
+                              className={`${
+                                isCollapisible ? "hidden" : "block"
+                              } inline`}
+                            >
+                              {title}
+                            </p>
+                          </Link>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="pl-10 flex items-center justify-between">
+            <div className="">
+              <Image
+                width={30}
+                height={30}
+                src="/images/userimage.png"
+                alt="user"
+                className="inline mr-3"
+              />
+              <span
+                className={`${
+                  isCollapisible ? "hidden" : "inline"
+                } text-gray-400 text-[15px]`}
+              >
+                Blessing Daniels
+              </span>
+            </div>
+            <DotIcon className={`${isCollapisible ? "hidden" : "inline"}`} />
+          </div>
         </div>
-      </div>
-      <div className="pl-10 flex items-center justify-between">
-        <div className="">
-          <Image
-            width={30}
-            height={30}
-            src="/images/userimage.png"
-            alt="user"
-            className="inline mr-3"
-          />
-          <span
-            className={`${
-              isCollapisible ? "hidden" : "inline"
-            } text-gray-400 text-[15px]`}
-          >
-            Blessing Daniels
-          </span>
-        </div>
-        <DotIcon className={`${isCollapisible ? "hidden" : "inline"}`} />
-      </div>
-    </div>
+      )}
+    </>
   );
 }
+
